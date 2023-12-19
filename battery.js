2ac4b74b3228caef99067ad29be8eefa4b17ac5f
MS <72651078+maddydevgits@users.noreply.github.com>
2023-12-19 22:30:43 +0530

    updating code

--- battery.js
+++ battery.js
@@ -8,27 +8,55 @@
         localStorage.setItem('batteryLevel', batteryLevel);
       }
 
-      function updateBatteryStatus() {
+    //   function updateBatteryStatus() {
+    //     // Update the battery status text
+    //     document.getElementById('batteryStatus').innerText = `Battery Level: ${batteryLevel}%`;
+
+    //     // Decrease the battery level by 1%
+    //     batteryLevel -= 1;
+
+    //     // Store the updated battery level in localStorage
+    //     localStorage.setItem('batteryLevel', batteryLevel);
+
+    //     // Check if the battery is still discharging
+    //     if (batteryLevel < 0) {
+    //       clearInterval(intervalId); // Stop the interval when the battery level is negative
+    //       document.getElementById('batteryStatus').innerText = 'Battery Level: 0%'; // Set to 0% if it goes below
+    //       batteryLevel=100;
+    //     }
+    //   }
+
+    //   // Call updateBatteryStatus every minute
+    //   const intervalId =setTimeout(updateBatteryStatus, 60000);
+    //   // Initial call to update battery status immediately
+    //   updateBatteryStatus();
+    let lastUpdateTimestamp = localStorage.getItem('lastUpdateTimestamp') || Date.now();
+
+    function updateBatteryStatus() {
+        // Calculate time difference since the last update
+        const currentTime = Date.now();
+        const timeDifference = currentTime - lastUpdateTimestamp;
+
+        // Update the battery level based on time elapsed (e.g., decrease by 1% every 60,000 milliseconds)
+        const decreasePercentage = Math.floor(timeDifference / 60000);
+        let batteryLevel = 100 - decreasePercentage;
+
+        // Ensure battery level is not negative
+        batteryLevel = Math.max(0, batteryLevel);
+
         // Update the battery status text
         document.getElementById('batteryStatus').innerText = `Battery Level: ${batteryLevel}%`;
 
-        // Decrease the battery level by 1%
-        batteryLevel -= 1;
-
-        // Store the updated battery level in localStorage
-        localStorage.setItem('batteryLevel', batteryLevel);
+        // Store the current timestamp in localStorage for the next update
+        localStorage.setItem('lastUpdateTimestamp', currentTime);
 
         // Check if the battery is still discharging
-        if (batteryLevel < 0) {
-          clearInterval(intervalId); // Stop the interval when the battery level is negative
-          document.getElementById('batteryStatus').innerText = 'Battery Level: 0%'; // Set to 0% if it goes below
-          batteryLevel=100;
+        if (batteryLevel > 0) {
+          // Schedule the next update using requestAnimationFrame
+          requestAnimationFrame(updateBatteryStatus);
         }
       }
-
-      // Call updateBatteryStatus every minute
-      const intervalId =setTimeout(updateBatteryStatus, 60000);
-      // Initial call to update battery status immediately
       updateBatteryStatus();
+
     });
   
